450592cfe560454d75bd93c2233440568dd7b2d2
Saurav Shrestha <sauravshrestha28@gmail.com>
2024-04-01 14:09:41 -0400

    comments

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,17 @@ import userController from '../controllers/userController.js'
 import appointmentController from '../controllers/appointmentController.js'
 import checker from '../middlewares/checker.js'
 
+// Dashboard Route
 router.get('/', userController.getDashboard)
+
+// Login / SignUp Routes
 router.get('/login', userController.getLogin)
 router.post('/login', userController.postLogin)
 router.get('/signup', userController.getSignup)
 router.post('/signup', userController.postSignup)
 router.post('/logout', userController.logout)
+
+// Driver Routes
 router.get('/gTest', checker.userTypeDriver, userController.getGTest)
 router.post('/gTest', userController.postGTest)
 router.get('/g2Test', checker.userTypeDriver, userController.getG2Test)
